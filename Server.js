4ac90d7a5f88d361c01c907885ab718097baf912
Amit vaghela <138572130+Amitkumar-Vaghela@users.noreply.github.com>
2025-05-10 22:09:37 +0530

    Delete Server.js

--- Server.js
+++
@@ -1,9 +0,0 @@
-let http = require("http");
-
-let server = http.createServer((req, res) => {
-  res.end("server is successfully running....");
-});
-
-server.listen(8000, () => {
-  console.log("Server is running on http://localhost:8000");
-});
